c190a71309225694892ecdf42165a68dd954eb64
Rico042002 <rico042002@gmail.com>
2022-02-17 23:40:15 -0600

    fourth commit

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ window.addEventListener('scroll', ()=>{
     const header = document.querySelector('header');
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+function toggleMenu() {
+    const menuToggle = document.querySelector('.menuToggle');
+    const nav = document.querySelector('.nav'); 
+    menuToggle.classList.toggle('active');
+    nav.classList.toggle('active');
+};
